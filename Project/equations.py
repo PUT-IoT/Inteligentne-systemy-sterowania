6ce3def86f2d555d79635410d21ded43f277f2b4
leszmak <n.szymon23@gmail.com>
2025-04-11 16:48:46 +0200

    Add equations

---
+++ Project/equations.py
@@ -0,0 +1,35 @@
+from const import  *
+from variable import *
+def calculate_new_A():
+
+    part_1 = k_m / (R_w + L_w / T_p)
+    part_2 = U_z + (L_w / T_p) * (U_pz / R_w) - k_e * omega_s
+
+    part_3 = (
+        (part_1 * part_2 - B * omega_s - M_obc) / R
+        - (M_pw - M_w - M_l) * G
+    )
+
+    part_4 = M_wir / 2 - (M_w + M_l + M_pw)
+
+    return part_3 / part_4
+
+def calculate_new_V_p():
+    return  V_p + A*T_p
+
+def calculate_new_H_p():
+    return H_p + V_p * T_p + A * T_p**2 / 2
+
+def calculate_new_omega_s():
+    return omega_s + A * T_p / R
+
+if __name__ == "__main__":
+    A = calculate_new_A()
+    print(f"Obliczone A: {A}")
+    omega_s = calculate_new_omega_s()
+    print(f"Obliczone omega_s: {omega_s}")
+    V_p = calculate_new_V_p()
+    print(f"Obliczone V_p: {V_p}")
+    H_p = calculate_new_H_p()
+    print(f"Obliczone H_p: {H_p}")
+    U_pz = U_z
---
+++ Project/variable.py
@@ -0,0 +1,6 @@
+M_l = 1 # masa ludzi i towaru w windzie (masa netto windy)
+U_z = 1 # napięcie zasilające wirnik
+U_pz = 1 # napięcie poprzednie zasilające wirnik
+omega_s = 1 # prędkość kątowa wirnika
+V_p = 1 # prędkość poprzednia windy
+H_p = 1 # wysokość poprzednia windy (czas próbkowania temu)
