41e96719320195ddf5b903e59d4b7fe0d01205cc
leszmak <n.szymon23@gmail.com>
2025-06-12 21:02:40 +0200

    Puszuje jak jest

--- Project/simpful_fuzzy_PD.py
+++ Project/simpful_fuzzy_PD.py
@@ -7,41 +7,44 @@ FS = FuzzySystem()
 
 # Funkcje przynależności - uchyb (e)
 FS.add_linguistic_variable("e", LinguisticVariable([
-    FuzzySet(function=Triangular_MF(-5.1, -4, -3), term="BDU"),
-    FuzzySet(function=Triangular_MF(-4, -3, -2), term="DU"),
-    FuzzySet(function=Triangular_MF(-3, -2, -1), term="ŚU"),
-    FuzzySet(function=Triangular_MF(-2, -1,  0), term="MU"),
-    FuzzySet(function=Triangular_MF(-1,  0,  1), term="Z"),
-    FuzzySet(function=Triangular_MF( 0,  1,  2), term="MD"),
-    FuzzySet(function=Triangular_MF( 1,  2,  3), term="ŚD"),
-    FuzzySet(function=Triangular_MF( 2,  3,  4), term="DD"),
-    FuzzySet(function=Triangular_MF( 3,  4,  5.1), term="BDD"),
-], universe_of_discourse=[-5.1, 5.1]))
+    FuzzySet(function=Triangular_MF(-10, -9, -8), term="BDU"),
+    FuzzySet(function=Triangular_MF(-9, -8, -7), term="DU"),
+    FuzzySet(function=Triangular_MF(-6, -4, -2), term="ŚU"),
+    FuzzySet(function=Triangular_MF(-4, -2,  0), term="MU"),
+    FuzzySet(function=Triangular_MF(-2,  0,  2), term="Z"),
+    FuzzySet(function=Triangular_MF( 0,  2,  4), term="MD"),
+    FuzzySet(function=Triangular_MF( 2,  4,  6), term="ŚD"),
+    FuzzySet(function=Triangular_MF( 7,  8,  9), term="DD"),
+    FuzzySet(function=Triangular_MF( 8,  9, 10), term="BDD"),
+], universe_of_discourse=[-10, 10]))
 
 # Funkcje przynależności - pochodna uchybu (de)
 FS.add_linguistic_variable("de", LinguisticVariable([
-    FuzzySet(function=Triangular_MF(-1.2, -1, -0.8), term="BDU"),
-    FuzzySet(function=Triangular_MF(-1, -0.75, -0.5), term="DU"),
-    FuzzySet(function=Triangular_MF(-0.75, -0.5, -0.25), term="ŚU"),
-    FuzzySet(function=Triangular_MF(-0.5, -0.25, 0), term="MU"),
-    FuzzySet(function=Triangular_MF(-0.1, 0, 0.1), term="Z"),
-    FuzzySet(function=Triangular_MF(0, 0.25, 0.5), term="MD"),
-    FuzzySet(function=Triangular_MF(0.25, 0.5, 0.75), term="ŚD"),
-    FuzzySet(function=Triangular_MF(0.5, 0.75, 1), term="DD"),
-    FuzzySet(function=Triangular_MF(0.8, 1, 1.2), term="BDD"),
-], universe_of_discourse=[-1.1, 1.1]))
+    FuzzySet(function=Triangular_MF(-0.3, -0.25, -0.2), term="BDU"),
+    FuzzySet(function=Triangular_MF(-0.25, -0.1875, -0.125), term="DU"),
+    FuzzySet(function=Triangular_MF(-0.1875, -0.125, -0.0625), term="ŚU"),
+    FuzzySet(function=Triangular_MF(-0.125, -0.0625, 0), term="MU"),
+    FuzzySet(function=Triangular_MF(-0.025, 0, 0.025), term="Z"),
+    FuzzySet(function=Triangular_MF(0, 0.0625, 0.125), term="MD"),
+    FuzzySet(function=Triangular_MF(0.0625, 0.125, 0.1875), term="ŚD"),
+    FuzzySet(function=Triangular_MF(0.125, 0.1875, 0.25), term="DD"),
+    FuzzySet(function=Triangular_MF(0.2, 0.25, 0.3), term="BDD"),
+], universe_of_discourse=[-0.275, 0.275]))
+
+
+
 
 # Funkcje przynależności - napięcie wyjściowe
 FS.add_linguistic_variable("u", LinguisticVariable([
-    FuzzySet(function=Triangular_MF(-120, -100, -80), term="BDU"),
+    FuzzySet(function=Triangular_MF(-120, -110, -100), term="BDU"),
     FuzzySet(function=Triangular_MF(-100, -80, -60), term="DU"),
-    FuzzySet(function=Triangular_MF(-80, -60, -40), term="ŚU"),
-    FuzzySet(function=Triangular_MF(-40, -20, 0), term="MU"),
-    FuzzySet(function=Triangular_MF(-10, 0, 10), term="Z"),
-    FuzzySet(function=Triangular_MF(0, 20, 40), term="MD"),
-    FuzzySet(function=Triangular_MF(40, 60, 80), term="ŚD"),
+    FuzzySet(function=Triangular_MF(-80, -70, -50), term="ŚU"),
+    FuzzySet(function=Triangular_MF(-50, -40, 5), term="MU"),
+    FuzzySet(function=Triangular_MF(-5, 0, 5), term="Z"),
+    FuzzySet(function=Triangular_MF(5, 40, 50), term="MD"),
+    FuzzySet(function=Triangular_MF(50, 70, 80), term="ŚD"),
     FuzzySet(function=Triangular_MF(60, 80, 100), term="DD"),
-    FuzzySet(function=Triangular_MF(80, 100, 120), term="BDD"),
+    FuzzySet(function=Triangular_MF(100, 110, 120), term="BDD"),
 ], universe_of_discourse=[-120, 120]))
 
 # Tabela reguł z przesłanego obrazka
